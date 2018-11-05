8d609b196525329e9af64eaf265ac78bae1e3ce8
Antonio Feregrino <antonio.feregrino@gmail.com>
2018-11-05 22:44:39 +0000

    Better features, better estimator.

--- pipelines.py
+++ pipelines.py
@@ -3,37 +3,38 @@ import numpy as np
 import string
 from sklearn.base import BaseEstimator, TransformerMixin
 
+
 class IsPunctuation(BaseEstimator, TransformerMixin):
     def __init__(self):
         self.punctuation = set(string.punctuation)
-        
+
     def fit(self, X, y=None):
         return self
 
-    def transform(self, X):        
+    def transform(self, X):
         punct = [False] * len(X)
         for i, t in enumerate(X):
             punct[i] = t in self.punctuation
-        return np.array(punct).reshape(-1,1)
+        return np.array(punct).reshape(-1, 1)
 
 
 class RelativeLocations(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
-        
+
     def fit(self, X, y=None):
         return self
 
-    def transform(self, X):        
-        return (X['token_position']/X['offer_length']).values.reshape(-1,1)
+    def transform(self, X):
+        return (X['loc'] / X['offer_len']).values.reshape(-1, 1)
 
 
 class Reshaper(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
-        
+
     def fit(self, X, y=None):
         return self
 
-    def transform(self, X):        
-        return X.values.reshape(-1,1)
+    def transform(self, X):
+        return X.values.reshape(-1, 1)
