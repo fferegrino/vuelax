bbd0e9c1d64ed68876179a1e8feba4199f6972d1
Antonio Feregrino <antonio.feregrino@gmail.com>
2018-11-08 22:25:05 +0000

    Kaggle dataset download.

--- make.sh
+++ make.sh
@@ -5,3 +5,10 @@ then
 	wget -O tagger/spanish.tagger https://github.com/fferegrino/vuelax/releases/download/0.1.0-circleci/spanish-distsim.tagger
 	wget -O tagger/stanford-postagger.jar https://github.com/fferegrino/vuelax/releases/download/0.1.0-circleci/stanford-postagger.jar
 fi
+
+if [ ! -d data ]
+then
+    mkdir data
+    pipenv run kaggle datasets download -p data -d ioexception/vuelax
+    unzip data/vuelax.zip -d data
+fi
