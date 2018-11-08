683bb86ebaa74490d9150e10603ae12bedb1318c
Antonio Feregrino <antonio.feregrino@gmail.com>
2018-11-08 22:01:49 +0000

    Moving to a shell file.

--- make.sh
+++ make.sh
@@ -1,4 +1,7 @@
-download_tagger:
+#!/usr/bin/env bash
+if [ ! -d tagger ]
+then
 	mkdir tagger
 	wget -O tagger/spanish.tagger https://github.com/fferegrino/vuelax/releases/download/0.1.0-circleci/spanish-distsim.tagger
 	wget -O tagger/stanford-postagger.jar https://github.com/fferegrino/vuelax/releases/download/0.1.0-circleci/stanford-postagger.jar
+fi
