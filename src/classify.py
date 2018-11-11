673c4067c14942a406a61a61703c4e15ad221804
Antonio Feregrino <antonio.feregrino@gmail.com>
2018-11-11 07:44:49 +0000

    Adding headers

--- src/classify.py
+++ src/classify.py
@@ -12,11 +12,7 @@ seed(42)
 
 
 def load_data():
-    training_set = pd.read_csv('data/i__training_data.csv',
-                               header=None, index_col=None,
-                               names=['sentence_id', 'offer_len', 'token', 'loc',
-                                      'pos', 'pos_left', 'pos_right',
-                                      'token_len', 'all_upper', 'n_tokens', 'real_label'])
+    training_set = pd.read_csv('../data/i__training_data.csv', index_col=None)
 
     training_set = training_set[~pd.isna(training_set['real_label'])]
 
