9781de33172e0ef20d155275bd578b36ad2a74f4
Antonio Feregrino <antonio.feregrino@gmail.com>
2018-11-11 07:50:23 +0000

    Point to the right file when classifying

--- src/classify.py
+++ src/classify.py
@@ -12,7 +12,7 @@ seed(42)
 
 
 def load_data():
-    training_set = pd.read_csv('../data/i__training_data.csv', index_col=None)
+    training_set = pd.read_csv('data/i__training_data.csv', index_col=None)
 
     training_set = training_set[~pd.isna(training_set['real_label'])]
 
@@ -37,8 +37,6 @@ def train_eval_algorithm(dump_to_disk=False):
 
     metrics, summary, cm = eval_summary(y_test, y_pred)
 
-
-
     if dump_to_disk:
         joblib.dump(pipeline, 'models/classify_pipeline.joblib')
 
@@ -46,7 +44,6 @@ def train_eval_algorithm(dump_to_disk=False):
 
 
 if __name__ == '__main__':
-
     metrics, summary, cm = train_eval_algorithm(True)
 
     with open('metrics.json', 'w') as w:
